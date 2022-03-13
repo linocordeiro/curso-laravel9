59ccc71e904cfa5faac86e5aaa794425c01a738e
Juscelino Cordeiro <jfilhodev@gmail.com>
2022-03-12 21:56:50 -0300

    add prettier e exclui readme

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    phpVersion: '8.1',
+    trailingCommaPHP: true,
+    braceStyle: 'psr-2',
+    printWidth: 120,
+};
